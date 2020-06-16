9371e5236a958271058f7f14f46ece138882390d
famurga <fmurga@unsa.edu.pe>
2020-06-16 04:00:57 -0500

    version1

---
+++ app/src/main/java/com/example/sensorvoz/ActivitySiguiente.java
@@ -0,0 +1,14 @@
+package com.example.sensorvoz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitySiguiente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siguiente);
+    }
+}
